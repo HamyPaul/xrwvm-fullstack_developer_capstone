5244eec1522a7b349113c0e35a009190a14bdd55
HamyPaul <141952042+HamyPaul@users.noreply.github.com>
2024-07-18 15:50:24 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = [
-    'localhost',
-    'https://hamypaulkmca-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+'localhost',
+'https://hamypaulkmca-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://hamypaulkmca-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+'https://hamypaulkmca-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -97,7 +97,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [{
     'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
