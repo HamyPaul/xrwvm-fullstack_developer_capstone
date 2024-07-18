05681a62ba825f3d666111ead46c987177253b97
HamyPaul <141952042+HamyPaul@users.noreply.github.com>
2024-07-18 14:26:23 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-#from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -13,11 +13,14 @@ urlpatterns = [
      path(route='register', view=views.registration, name='register'),
      path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
      path(
-         route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+         route='get_dealers/<str:state>', view=views.get_dealerships, 
+          name='get_dealers_by_state'),
      path(
-         route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+         route='dealer/<int:dealer_id>', view=views.get_dealer_details, 
+          name='dealer_details'),
      path(
-         route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+         route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, 
+          name='dealer_details'),
      path(route='add_review', view=views.add_review, name='add_review'),
      path(route='get_cars', view=views.get_cars, name='get_cars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
